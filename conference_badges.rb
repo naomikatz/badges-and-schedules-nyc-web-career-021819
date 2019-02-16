987dc4c74f00f188946a901ccf4f1249c99d5827
Naomi Katz <naykatz@gmail.com>
2019-02-16 19:20:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+end
