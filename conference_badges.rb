c8feee29fa7a6195ab21fa3601b2606f7403a515
Naomi Katz <naykatz@gmail.com>
2019-02-16 19:27:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,6 @@ end
 
 def assign_rooms(names)
   names.each_with_index.msp do |name, room|
-    room_assignment << "Hello, #{name}! You'll be assigned to room #{room + 1}!"
+    "Hello, #{name}! You'll be assigned to room #{room + 1}!"
   end
-  return room_assigment
 end
