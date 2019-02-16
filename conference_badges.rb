2a857bfdc74bc44ded2909d33d5286a33e971b1d
Naomi Katz <naykatz@gmail.com>
2019-02-16 19:25:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(names)
   room_assignment = []
-  array.each_with_index do |name, room|
+  names.each_with_index do |name, room|
     room_assignment << "Hello, #{name}! You'll be assigned to room #{room + 1}!"
   end
   return room_assigment
