a07456f8b17f5a6173273e7145caca6d0d9c60f2
Naomi Katz <naykatz@gmail.com>
2019-02-16 19:20:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,21 @@
 # Write your code here.
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(names)
+  batch_message_array = []
   array.each do |name|
-    puts "Hello, my name is #{name}."
+    batch_message_array << badge_maker(name)
   end
+  return batch_messsage_array
 end
 
-def assign_rooms(array)
+def assign_rooms(names)
+  room_assignment = []
+  array.each_with_index do |name, room|
+    room_assignment << "Hello, #{name}! You'll be assigned to room #{room + 1}!"
+  end
+  return room_assigment
 end
